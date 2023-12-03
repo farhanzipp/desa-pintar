0c05af14db585d768346628abb2d6281d14aefd9
farhanzipp <farhanaji317@gmail.com>
2023-12-03 20:34:31 +0700

    feat: add features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
+import Features from './components/Features.jsx';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
-      
+      <Features />
     </>
   )
 }
---
+++ src/components/Features.jsx
@@ -0,0 +1,32 @@
+import FeatureCard from "./FeatureCard"
+
+const Features = () => {
+    return (
+        <section>
+            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+                <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+                    <div>
+                        <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+                            Brand new
+                        </p>
+                    </div>
+                    <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+                        quick, brown fox jumps over a lazy dog
+                    </h2>
+                    <p className="text-base text-gray-700 md:text-lg">
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                        accusantium doloremque rem aperiam, eaque ipsa quae.
+                    </p>
+                </div>
+                <div className="grid gap-4 row-gap-5 sm:grid-cols-2 lg:grid-cols-4">
+                    <FeatureCard />
+                    <FeatureCard />
+                    <FeatureCard />
+                    <FeatureCard />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Features
